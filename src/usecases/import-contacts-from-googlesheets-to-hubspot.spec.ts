662474817652544e65ae2aa579517558753b82fb
Rafhael Gomes <rafhael@protonmail.com>
2022-10-06 17:35:22 -0300

    test: ensure ImportContactsFromGoogleSheetsToHubspot is defined

---
+++ src/usecases/import-contacts-from-googlesheets-to-hubspot.spec.ts
@@ -0,0 +1,7 @@
+import { ImportContactsFromGoogleSheetsToHubspot } from '@/usecases/import-contacts-from-googlesheets-to-hubspot'
+
+describe('ImportContactsFromGoogleSheetsToHubspot Unit Test', () => {
+  it('should be defined', () => {
+    expect(new ImportContactsFromGoogleSheetsToHubspot()).toBeDefined()
+  })
+})
