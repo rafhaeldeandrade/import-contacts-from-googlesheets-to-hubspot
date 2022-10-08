c9c36c4d5521a5b163ce65f407971e921089ec10
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 10:09:55 -0300

    feat: ensure GoogleapisFetchContactsFromGoogleSheets calls google.sheets with correct params

--- src/infra/googleapis-fetch-contacts-from-google-sheets.ts
+++ src/infra/googleapis-fetch-contacts-from-google-sheets.ts
@@ -1,9 +1,18 @@
+import { google } from 'googleapis'
+import { OAuth2Client } from 'google-auth-library'
+
 import { Contact, FetchContactsFromGoogleSheets } from '@/usecases/contracts'
 
 export class GoogleapisFetchContactsFromGoogleSheets
   implements FetchContactsFromGoogleSheets
 {
+  constructor(private readonly oauth2client: OAuth2Client) {}
+
   async fetch(spreadsheetId: string, pageName: string): Promise<Contact[]> {
+    google.sheets({
+      version: 'v4',
+      auth: this.oauth2client,
+    })
     return []
   }
 }
