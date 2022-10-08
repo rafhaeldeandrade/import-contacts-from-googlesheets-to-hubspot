221886d52c1cdd77a236092d92b13bfe3564aa56
Rafhael Gomes <rafhael@protonmail.com>
2022-10-07 22:04:26 -0300

    feat: ensure ImportContactsFromGoogleSheetsToHubspot throws if pageName isnt provided

--- src/usecases/import-contacts-from-googlesheets-to-hubspot.ts
+++ src/usecases/import-contacts-from-googlesheets-to-hubspot.ts
@@ -2,11 +2,17 @@ import { MissingParamError } from '@/usecases/errors'
 
 interface ImportContactsFromGoogleSheetsToHubspotInput {
   spreadsheetId: string
+  pageName: string
 }
 
 export class ImportContactsFromGoogleSheetsToHubspot {
   execute(params: ImportContactsFromGoogleSheetsToHubspotInput): void {
-    const { spreadsheetId } = params
+    this.validateParams(params)
+  }
+
+  validateParams(params: ImportContactsFromGoogleSheetsToHubspotInput): void {
+    const { spreadsheetId, pageName } = params
     if (!spreadsheetId) throw new MissingParamError('spreadsheetId')
+    if (!pageName) throw new MissingParamError('pageName')
   }
 }
