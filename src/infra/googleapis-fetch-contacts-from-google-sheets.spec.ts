75e1cc1a5bf48fa3c78065216ca5eb3c3aed69f2
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 10:10:09 -0300

    test: ensure GoogleapisFetchContactsFromGoogleSheets calls google.sheets with correct params

--- src/infra/googleapis-fetch-contacts-from-google-sheets.spec.ts
+++ src/infra/googleapis-fetch-contacts-from-google-sheets.spec.ts
@@ -1,13 +1,26 @@
+import { google } from 'googleapis'
+import { OAuth2Client } from 'google-auth-library'
+import { faker } from '@faker-js/faker'
+
 import { GoogleapisFetchContactsFromGoogleSheets } from '@/infra/googleapis-fetch-contacts-from-google-sheets'
 
+class OAuth2ClientStub extends OAuth2Client {}
 interface SutTypes {
   sut: GoogleapisFetchContactsFromGoogleSheets
 }
 
 function makeSut(): SutTypes {
-  const sut = new GoogleapisFetchContactsFromGoogleSheets()
+  const oauth2ClientStub = new OAuth2ClientStub()
+  const sut = new GoogleapisFetchContactsFromGoogleSheets(oauth2ClientStub)
   return {
     sut,
+  }
+}
+
+function makeProps() {
+  return {
+    spreadsheetId: faker.datatype.uuid(),
+    pageName: faker.lorem.words(1),
   }
 }
 
@@ -15,5 +28,15 @@ describe('GoogleapisFetchContactsFromGoogleSheets', () => {
   it('should be defined', async () => {
     const { sut } = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('should call google.sheets with correct params', async () => {
+    const { sut } = makeSut()
+    const sheetsSpy = jest.spyOn(google, 'sheets')
+    const { spreadsheetId, pageName } = makeProps()
+    await sut.fetch(spreadsheetId, pageName)
+    expect(sheetsSpy).toHaveBeenCalledWith({
+      auth: expect.any(OAuth2Client),
+    })
   })
 })
