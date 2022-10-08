e46be58e324ce2e0f18f4a5f404f996ea05f2668
Rafhael Gomes <rafhael@protonmail.com>
2022-10-07 22:01:59 -0300

    test: ensure ImportContactsFromGoogleSheetsToHubspot throws if spreadsheetId isnt provided

--- src/usecases/import-contacts-from-googlesheets-to-hubspot.ts
+++ src/usecases/import-contacts-from-googlesheets-to-hubspot.ts
@@ -1,1 +1,12 @@
-export class ImportContactsFromGoogleSheetsToHubspot {}
+import { MissingParamError } from '@/usecases/errors'
+
+interface ImportContactsFromGoogleSheetsToHubspotInput {
+  spreadsheetId: string
+}
+
+export class ImportContactsFromGoogleSheetsToHubspot {
+  execute(params: ImportContactsFromGoogleSheetsToHubspotInput): void {
+    const { spreadsheetId } = params
+    if (!spreadsheetId) throw new MissingParamError('spreadsheetId')
+  }
+}
