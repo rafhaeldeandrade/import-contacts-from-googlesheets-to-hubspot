680e78f84a0b61c3f673c19983f1f9d6540e50d7
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:32:05 -0300

    feat: ensure PslRetrieveWebsiteDomain calls psl.parse with correct param

--- src/infra/psl-retrieve-website-domain.ts
+++ src/infra/psl-retrieve-website-domain.ts
@@ -1,7 +1,9 @@
 import { RetrieveWebsiteDomain } from '@/usecases/contracts'
+import psl, { ParsedDomain } from 'psl'
 
 export class PslRetrieveWebsiteDomain implements RetrieveWebsiteDomain {
   retrieve(websiteUrl: string): string {
+    psl.parse(websiteUrl) as ParsedDomain
     return ''
   }
 }
