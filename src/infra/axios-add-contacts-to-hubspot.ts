1befaceb6738572da942870a1838d9eac61e2c13
Rafhael Gomes <rafhael@protonmail.com>
2022-10-09 01:19:15 -0300

    feat: ensure AxiosAddContactsToHubspot calls axios.post with correct params

--- src/infra/axios-add-contacts-to-hubspot.ts
+++ src/infra/axios-add-contacts-to-hubspot.ts
@@ -1,7 +1,53 @@
+import axios from 'axios'
+
+import env from '@/main/config/env'
 import { AddContactsToHubspot, Contact } from '@/usecases/contracts'
+
+type HubspotContactProperties = {
+  property: string
+  value: string
+}
+interface HubspotContact {
+  email: string
+  properties: HubspotContactProperties[]
+}
 
 export class AxiosAddContactsToHubspot implements AddContactsToHubspot {
   async add(contacts: Contact[]): Promise<void> {
+    const hubspotContacts = this.generateRequestBody(contacts)
+    await axios.post(
+      'https://api.hubapi.com/contacts/v1/contact/batch/',
+      hubspotContacts,
+      {
+        headers: {
+          Authorization: `Bearer ${env.hubspotApiKey}`,
+        },
+      }
+    )
     return Promise.resolve()
+  }
+
+  generateRequestBody(contacts: Contact[]): HubspotContact[] {
+    return contacts.map((contact) => ({
+      email: contact.email,
+      properties: [
+        {
+          property: 'firstname',
+          value: contact.name,
+        },
+        {
+          property: 'website',
+          value: contact.website,
+        },
+        {
+          property: 'phone',
+          value: contact.phone,
+        },
+        {
+          property: 'company',
+          value: contact.company,
+        },
+      ],
+    }))
   }
 }
