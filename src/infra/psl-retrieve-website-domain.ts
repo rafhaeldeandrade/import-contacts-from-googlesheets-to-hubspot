d4514dd76a6f39a351155af3b9e30755f9959b46
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:28:34 -0300

    feat: ensure PslRetrieveWebsiteDomain is defined

---
+++ src/infra/psl-retrieve-website-domain.ts
@@ -0,0 +1,7 @@
+import { RetrieveWebsiteDomain } from '@/usecases/contracts'
+
+export class PslRetrieveWebsiteDomain implements RetrieveWebsiteDomain {
+  retrieve(websiteUrl: string): string {
+    return ''
+  }
+}
