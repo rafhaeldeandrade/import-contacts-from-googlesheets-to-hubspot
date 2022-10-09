19a7acb084b57a810154dd81b984f1d27ccaceef
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:15:09 -0300

    test: ensure GoogleapisFetchContactsFromGoogleSheets returns Contact[] or empty array on success

--- src/infra/googleapis-fetch-contacts-from-google-sheets.spec.ts
+++ src/infra/googleapis-fetch-contacts-from-google-sheets.spec.ts
@@ -3,6 +3,43 @@ import { OAuth2Client } from 'google-auth-library'
 import { faker } from '@faker-js/faker'
 
 import { GoogleapisFetchContactsFromGoogleSheets } from '@/infra/googleapis-fetch-contacts-from-google-sheets'
+jest.mock('googleapis', () => ({
+  google: {
+    sheets: jest.fn().mockImplementation(() => ({
+      spreadsheets: {
+        values: {
+          get: () => ({
+            data: {
+              values: [
+                [
+                  'Nome da empresa',
+                  'Nome completo',
+                  'Email',
+                  'Telefone',
+                  'Website',
+                ],
+                [
+                  faker.company.name(),
+                  faker.name.fullName(),
+                  faker.internet.email(),
+                  faker.phone.number(),
+                  faker.internet.url(),
+                ],
+                [
+                  faker.company.name(),
+                  faker.name.fullName(),
+                  faker.internet.email(),
+                  faker.phone.number(),
+                  faker.internet.url(),
+                ],
+              ],
+            },
+          }),
+        },
+      },
+    })),
+  },
+}))
 
 class OAuth2ClientStub extends OAuth2Client {}
 interface SutTypes {
@@ -37,6 +74,59 @@ describe('GoogleapisFetchContactsFromGoogleSheets', () => {
     await sut.fetch(spreadsheetId, pageName)
     expect(sheetsSpy).toHaveBeenCalledWith({
       auth: expect.any(OAuth2Client),
+      version: 'v4',
     })
+  })
+
+  it('should return Contact[] on success', async () => {
+    const { sut } = makeSut()
+    const { spreadsheetId, pageName } = makeProps()
+    const contacts = await sut.fetch(spreadsheetId, pageName)
+    expect(contacts).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          company: expect.any(String),
+          name: expect.any(String),
+          email: expect.any(String),
+          phone: expect.any(String),
+          website: expect.any(String),
+        }),
+      ])
+    )
+  })
+
+  it('should return empty array if values is empty', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(google, 'sheets').mockImplementationOnce(
+      () =>
+        ({
+          spreadsheets: {
+            values: {
+              get: () => ({
+                data: {
+                  values: [],
+                },
+              }),
+            },
+          },
+        } as any)
+    )
+    const { spreadsheetId, pageName } = makeProps()
+    const contacts = await sut.fetch(spreadsheetId, pageName)
+    expect(contacts).toEqual([])
+  })
+
+  it('should throw if google.sheets throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(google, 'sheets').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const { spreadsheetId, pageName } = makeProps()
+    const promise = sut.fetch(spreadsheetId, pageName)
+    await expect(promise).rejects.toThrow(
+      new Error(
+        'Something went wrong while fetching contacts from Google Sheets, try again later and be sure to have first row with the following headers: Nome da empresa, Nome completo, Email, Telefone, Website and subsequent rows filled with related data'
+      )
+    )
   })
 })
