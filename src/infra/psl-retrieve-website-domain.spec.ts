db57df383614d65d6ba52060b193cc8fadcaae63
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:28:45 -0300

    test: ensure PslRetrieveWebsiteDomain is defined

---
+++ src/infra/psl-retrieve-website-domain.spec.ts
@@ -0,0 +1,7 @@
+import { PslRetrieveWebsiteDomain } from '@/infra/psl-retrieve-website-domain'
+
+describe('PslRetrieveWebsiteDomain', () => {
+  it('should be defined', () => {
+    expect(new PslRetrieveWebsiteDomain()).toBeDefined()
+  })
+})
