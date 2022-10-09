8a97fe54163cc77787e041d0694597b7e8d39dfd
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:32:19 -0300

    test: ensure PslRetrieveWebsiteDomain calls psl.parse with correct param

--- src/infra/psl-retrieve-website-domain.spec.ts
+++ src/infra/psl-retrieve-website-domain.spec.ts
@@ -1,7 +1,20 @@
+import psl from 'psl'
+import { faker } from '@faker-js/faker'
+
 import { PslRetrieveWebsiteDomain } from '@/infra/psl-retrieve-website-domain'
 
 describe('PslRetrieveWebsiteDomain', () => {
   it('should be defined', () => {
-    expect(new PslRetrieveWebsiteDomain()).toBeDefined()
+    const sut = new PslRetrieveWebsiteDomain()
+    expect(sut).toBeDefined()
+  })
+
+  it('should call psl.parse with correct param', () => {
+    const sut = new PslRetrieveWebsiteDomain()
+    const websiteUrl = faker.internet.url()
+    const parseSpy = jest.spyOn(psl, 'parse').mockReturnValueOnce(null as any)
+    sut.retrieve(websiteUrl)
+    expect(parseSpy).toHaveBeenCalledTimes(1)
+    expect(parseSpy).toHaveBeenCalledWith(websiteUrl)
   })
 })
