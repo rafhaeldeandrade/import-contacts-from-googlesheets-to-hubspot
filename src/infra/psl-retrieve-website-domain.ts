51f85645439411186bdb55573c7511598bc8b0d4
Rafhael Gomes <rafhael@protonmail.com>
2022-10-08 23:40:44 -0300

    feat: ensure PslRetrieveWebsiteDomain returns domain on success

--- src/infra/psl-retrieve-website-domain.ts
+++ src/infra/psl-retrieve-website-domain.ts
@@ -4,9 +4,10 @@ import psl, { ParsedDomain } from 'psl'
 export class PslRetrieveWebsiteDomain implements RetrieveWebsiteDomain {
   retrieve(websiteUrl: string): string {
     try {
-      psl.parse(websiteUrl) as ParsedDomain
-      return ''
+      const parsed = psl.parse(websiteUrl) as ParsedDomain
+      return parsed.domain as string
     } catch (e) {
+      console.log(e)
       throw new Error('Something went wrong, check your url and try again')
     }
   }
